f2f144e0e5aed722913ef2aeb0e5303bbc8408c7
John Henderson <john.henderson@energyweb.org>
2020-12-11 14:47:56 -0800

    Removed internal addresses in example config

--- examples/server/express/passport.js
+++ examples/server/express/passport.js
@@ -12,7 +12,7 @@ const jwtOptions = {
   secretOrKey: jwtSecret
 }
 module.exports.preparePassport = () => {
-  passport.use(new LoginStrategy({ jwtSecret, name: LOGIN_STRATEGY, rpcUrl: 'https://volta-rpc-vkn5r5zx4ke71f9hcu0c.energyweb.org/', cacheServerUrl: 'http://13.52.78.249:3333/' }))
+  passport.use(new LoginStrategy({ jwtSecret, name: LOGIN_STRATEGY, rpcUrl: 'https://volta-rpc.energyweb.org/', cacheServerUrl: 'https://volta-iam-cacheserver.energyweb.org/' }))
   passport.use(new Strategy(jwtOptions, function (payload, done) {
     return done(null, payload)
   }))
