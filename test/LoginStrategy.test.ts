5c75680133653bc22cddb2bd995e39f9ebe2da93
knzeng-e <kevin.nzeng@gmail.com>
2021-05-31 11:41:39 +0200

    feat(LoginStrategy.test): adding unit tests with a second claim/issuer

--- test/LoginStrategy.test.ts
+++ test/LoginStrategy.test.ts
@@ -1,21 +1,23 @@
 import assert from 'assert';
 import { config } from 'dotenv';
 import { Keys } from '@ew-did-registry/keys'
+import { ClaimsUser } from '@ew-did-registry/claims';
 import { ClaimData } from './claim-creation/ClaimData';
 import { ITokenPayload } from '../lib/LoginStrategy.types';
 import { ClaimsUserFactory } from './claim-creation/ClaimsUserFactory';
 import { LoginStrategy, LoginStrategyOptions } from '../lib/LoginStrategy';
-import { ClaimsUser, ClaimsIssuer, ClaimsVerifier } from '@ew-did-registry/claims';
 import { providers } from 'ethers';
 
 config();
 
-let claimToken: string;
+let firstClaimToken: string;
 let claimData: ClaimData;
 let payload: ITokenPayload;
 let claimsUser: ClaimsUser;
+let secondClaimToken: string;
+let otherclaimsUser: ClaimsUser;
 let loginProcess: LoginStrategy;
-let unAuthenticatedPayload: ITokenPayload;
+let secondPayload: ITokenPayload;
 
 const initParams: LoginStrategyOptions = {
     jwtSecret: process.env.PRIVATE_PEM,
@@ -30,11 +32,14 @@ const initParams: LoginStrategyOptions = {
   }
 
 const provider = new providers.JsonRpcProvider(initParams.rpcUrl)
-const issuerDID = 'did:ethr:0x72EFf9faB7876c4c1b6cAe426c121358431758F3'
 const keys = new Keys({
     privateKey: process.env.PRIVATE_KEY,
   });
-  const otherDID = `did:ethr:${keys.getAddress()}`
+  const secondKeys = new Keys({
+    privateKey: process.env.PRIVATE_KEY_2,
+  });
+  const issuerDID = `did:ethr:${keys.getAddress()}`
+  const secondDID = `did:ethr:${secondKeys.getAddress()}`
 
 describe("Testing LoginStrategy", () => {
     
@@ -43,31 +48,43 @@ describe("Testing LoginStrategy", () => {
             claimType: "user.roles.example1.apps.john.iam.ewc",
           }
         claimsUser = ClaimsUserFactory.create(keys)
+        otherclaimsUser = ClaimsUserFactory.create(secondKeys)
         loginProcess = new LoginStrategy(initParams);
-        claimToken = await claimsUser.createPublicClaim(claimData);
+        firstClaimToken = await claimsUser.createPublicClaim(claimData);
+        secondClaimToken = await otherclaimsUser.createPublicClaim(claimData);
         payload = {
             claimData : {
                 blockNumber: await provider.getBlockNumber() //TO-DO: Verify this parameter in specs 
             },
             iss: issuerDID,
-            sub: otherDID
+            sub: secondDID
         }
-        unAuthenticatedPayload = {
+        secondPayload = {
             claimData : {
                 blockNumber: await provider.getBlockNumber() //TO-DO: Verify this parameter in specs 
             },
-            iss: otherDID,
-            sub: otherDID
+            iss: secondDID,
+            sub: secondDID
         }
     })
 
-    it("should verify DID authentication in DID document", async () => {
-          const did = await loginProcess.verifyDidClaim(claimToken, payload)
-       assert.strictEqual(did, issuerDID)
+    it("should authenticate first DID with first Claim issuer", async () => {
+          const did = await loginProcess.verifyDidClaim(firstClaimToken, payload)
+          assert.strictEqual(did, issuerDID)
     })
 
-    it("should reject the wrong DID issuer", async () => {
-        const did = await loginProcess.verifyDidClaim(claimToken, unAuthenticatedPayload)
-     assert.notDeepStrictEqual(did, issuerDID)
-  })
+    it("should reject the second DID authentication with first Claim issuer", async () => {
+        const did = await loginProcess.verifyDidClaim(secondClaimToken, payload)
+          assert.strictEqual(did, null)
+    })
+
+    it("should reject first DID with second Claim issuer", async () => {
+      const did = await loginProcess.verifyDidClaim(firstClaimToken, secondPayload)
+      assert.strictEqual(did, null)
+    })
+
+    it("should authenticate second DID with second Claim issuer", async () => {
+      const did = await loginProcess.verifyDidClaim(secondClaimToken, secondPayload)
+      assert.strictEqual(did, secondDID)
+    })
 })
