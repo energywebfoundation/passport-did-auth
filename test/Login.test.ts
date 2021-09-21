2c55ec5d9bd54b723e7bcb9c11f89fd22b574918
John Henderson <john.henderson@energyweb.org>
2021-09-21 17:20:07 +0200

    Login.test.ts: remove commented code

--- test/Login.test.ts
+++ test/Login.test.ts
@@ -1,16 +1,9 @@
 import {
     Wallet,
-    Contract,
     providers,
-    ContractFactory,
 } from "ethers";
 
 import { getServer } from './testUtils/server';
-
-import {
-    abi as didContractAbi,
-    bytecode as didContractBytecode,
-} from "./testUtils/ERC1056.json";
 
 import {
     setChainConfig,
@@ -28,7 +21,6 @@ const GANACHE_PORT = 8544;
 const rpcUrl = `http://localhost:${GANACHE_PORT}`;
 
 const provider = new providers.JsonRpcProvider(rpcUrl);
-const deployer = provider.getSigner(0);
 
 // funded private key for 0x627306090abaB3A6e1400e9345bC60c78a8BEf57
 // ganache with "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat" mnemonic
@@ -78,31 +70,14 @@ beforeAll(async () => {
 })
 
 it('Can Log in',  async () => {
-    // const userAddress = new Wallet(userPrivKey).address;
-    // const did = `did:ethr:${userAddress}`;
-    // const identityToken = await createIdentityProofWithDelegate(userPrivKey, rpcUrl, did);
-    // const idendity = {
-    //     token: "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiIiLCJjbGFpbURhdGEiOnsiYmxvY2tOdW1iZXIiOjMyMTM5fSwic3ViIjoiIiwiaWF0IjoxNjMyMTUxNzAyMTUzfQ.MHg0MjMwZjdlZTcwOGQ2MzA3Yjk3ZWJkN2RhNGNkN2QzM2QyNjI1ZjM0MGUyNzhmODY1NGUwMDc1MTBjY2RiYTVjMWFhNWJlMGQ1NGI1YWViNWNmOGUwZWI4ZDg5YmNjNmI4MTAzYmFiZjdlOTJlMDQzNjhjMWEzYTMxYTNkMDY2YjFj",
-    //     payload: {
-    //         iss: 'did:ethr:0xC5fdf4076b8F3A5357c5E395ab970B5B54098Fef',
-    //         claimData: { blockNumber: 32139 },
-    //         sub: 'did:ethr:0xC5fdf4076b8F3A5357c5E395ab970B5B54098Fef',
-    //         iat: 1632151702153
-    //       },
-    // }
+    // TODO: instead of createIdentityProof (or need another test)
+    // Create an asset, create a key, add to asset, create IdentityProofWithDelegate....
     const identityToken = await iam.createIdentityProof();
-    // const loginStrategyOptions : LoginStrategyOptions = {
-    //     jwtSecret: private_pem_secret,
-    //     name: LOGIN_STRATEGY,
-    //     rpcUrl: `http://localhost:8544`,
-    //     didContractAddress: didContract.address
-    // }
-    // const loginStrategy = new LoginStrategy(loginStrategyOptions)
-    // eslint-disable-next-line @typescript-eslint/no-empty-function
-    //await loginStrategy.validate(identityToken.token, identityToken.payload, ()=>{})
 
     await request(getServer(didContract.address))
         .post('/login')
         .send({identityToken})
         .expect(200);
+        // TODO: expect jwt token
+        // TODO: ensure that test ends afterwards
 });
