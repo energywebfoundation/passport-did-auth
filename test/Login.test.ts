06074dffe278fafc4aac20c5b71d480bdbfa789d
John Henderson <john.henderson@energyweb.org>
2021-09-21 16:53:50 +0200

    test(LoginStrat): importing contracts to init iam-client-lib

--- test/Login.test.ts
+++ test/Login.test.ts
@@ -22,8 +22,7 @@ import request from 'supertest';
 import { JWT } from "@ew-did-registry/jwt";
 import { LoginStrategy, LoginStrategyOptions } from "../lib/LoginStrategy";
 import { LOGIN_STRATEGY, private_pem_secret } from "./testUtils/preparePassport";
-
-let didContract: Contract;
+import { assetsManager, claimManager, deployClaimManager, deployDidRegistry, deployEns, deployIdentityManager, didContract, domainNotifer, ensRegistry, ensResolver } from "./setup_contracts";
 
 const GANACHE_PORT = 8544;
 const rpcUrl = `http://localhost:${GANACHE_PORT}`;
@@ -34,11 +33,6 @@ const deployer = provider.getSigner(0);
 // funded private key for 0x627306090abaB3A6e1400e9345bC60c78a8BEf57
 // ganache with "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat" mnemonic
 const userPrivKey = '0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3';
-
-const deployDidRegistry = async (): Promise<void> => {
-    const didContractFactory = new ContractFactory(didContractAbi, didContractBytecode, deployer);
-    didContract = await didContractFactory.deploy();
-};
 
 export const createIdentityProofWithDelegate = async (secp256k1PrivateKey: string, rpcUrl: string, identityProofDid: string) => {
     const provider = new providers.JsonRpcProvider(rpcUrl);
@@ -64,21 +58,29 @@ let iam: IAM;
 
 beforeAll(async () => {
     await deployDidRegistry();
+    await deployEns();
+    await deployClaimManager();
+    await deployIdentityManager();
 
-    // const { chainId } = await provider.getNetwork();
-    // setChainConfig(chainId, {
-    //     rpcUrl,
-    //     didContractAddress: didContract.address,
-    // });
-    // // setCacheClientOptions(chainId, { url: "" });
-    // iam = new IAM({privateKey: userPrivKey, rpcUrl});
-    // await iam.initializeConnection({initCacheServer: false})
+    const { chainId } = await provider.getNetwork();
+    setChainConfig(chainId, {
+        rpcUrl,
+        didContractAddress: didContract.address,
+        ensRegistryAddress: ensRegistry.address,
+        ensResolverAddress: ensResolver.address,
+        assetManagerAddress: assetsManager.address,
+        claimManagerAddress: claimManager.address,
+        domainNotifierAddress: domainNotifer.address
+    });
+    // setCacheClientOptions(chainId, { url: "" });
+    iam = new IAM({privateKey: userPrivKey, rpcUrl});
+    await iam.initializeConnection({initCacheServer: false})
 })
 
 it('Can Log in',  async () => {
-    const userAddress = new Wallet(userPrivKey).address;
-    const did = `did:ethr:${userAddress}`;
-    const identityToken = await createIdentityProofWithDelegate(userPrivKey, rpcUrl, did);
+    // const userAddress = new Wallet(userPrivKey).address;
+    // const did = `did:ethr:${userAddress}`;
+    // const identityToken = await createIdentityProofWithDelegate(userPrivKey, rpcUrl, did);
     // const idendity = {
     //     token: "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiIiLCJjbGFpbURhdGEiOnsiYmxvY2tOdW1iZXIiOjMyMTM5fSwic3ViIjoiIiwiaWF0IjoxNjMyMTUxNzAyMTUzfQ.MHg0MjMwZjdlZTcwOGQ2MzA3Yjk3ZWJkN2RhNGNkN2QzM2QyNjI1ZjM0MGUyNzhmODY1NGUwMDc1MTBjY2RiYTVjMWFhNWJlMGQ1NGI1YWViNWNmOGUwZWI4ZDg5YmNjNmI4MTAzYmFiZjdlOTJlMDQzNjhjMWEzYTMxYTNkMDY2YjFj",
     //     payload: {
@@ -88,19 +90,19 @@ it('Can Log in',  async () => {
     //         iat: 1632151702153
     //       },
     // }
-    //const identityToken = await iam.createIdentityProof();
-    const loginStrategyOptions : LoginStrategyOptions = {
-        jwtSecret: private_pem_secret,
-        name: LOGIN_STRATEGY,
-        rpcUrl: `http://localhost:8544`,
-        didContractAddress: didContract.address
-    }
-    const loginStrategy = new LoginStrategy(loginStrategyOptions)
+    const identityToken = await iam.createIdentityProof();
+    // const loginStrategyOptions : LoginStrategyOptions = {
+    //     jwtSecret: private_pem_secret,
+    //     name: LOGIN_STRATEGY,
+    //     rpcUrl: `http://localhost:8544`,
+    //     didContractAddress: didContract.address
+    // }
+    // const loginStrategy = new LoginStrategy(loginStrategyOptions)
     // eslint-disable-next-line @typescript-eslint/no-empty-function
-    await loginStrategy.validate(identityToken.token, identityToken.payload, ()=>{})
+    //await loginStrategy.validate(identityToken.token, identityToken.payload, ()=>{})
 
-    // await request(getServer(didContract.address))
-    //     .post('/login')
-    //     .send({identity: identityToken})
-    //     .expect(200);
+    await request(getServer(didContract.address))
+        .post('/login')
+        .send({identityToken})
+        .expect(200);
 });
