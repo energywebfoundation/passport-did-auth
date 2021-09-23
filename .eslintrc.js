71f7c71a08833f1f0b8c66478ad57c1f4917a200
John Henderson <john.henderson@energyweb.org>
2021-09-23 18:41:40 +0200

    eslint: remove no-extraneous-dependencies for devDependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,7 +27,7 @@ module.exports = {
     'import/no-extraneous-dependencies': [
       "error",
       {
-        "devDependencies": false,
+        "devDependencies": true,
         "optionalDependencies": false,
         "peerDependencies": false,
       },
