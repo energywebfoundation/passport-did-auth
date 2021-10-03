3c40401fd5f529ba60492e57442bbfe218dd0a02
John Henderson <jrhender@users.noreply.github.com>
2021-10-03 19:12:49 +0200

    fix(AuthTokenVerifier): restore recovery of address from sig (#133)
    Try to login by recovering address and by considering the publicKeys in the DID Document.
    In other words, even if the DID Document does not explicitly allow an address "recovery", attempt it anyways.
    Tests are also refactored to cover this scenario.
    Jira item: PDA-14
    
    Co-authored-by: Dmitry Fesenko <dmitry.fesenko@482.solutions>

--- test/TestDidDocuments.ts
+++ test/TestDidDocuments.ts
@@ -1,83 +1,36 @@
-import { Keys } from '@ew-did-registry/keys'
-import {IDIDDocument} from "@ew-did-registry/did-resolver-interface"
+import { Methods } from "@ew-did-registry/did";
+import {
+  IDIDDocument,
+  IPublicKey,
+  KeyTags,
+  PubKeyType,
+} from "@ew-did-registry/did-resolver-interface";
+import { Wallet, utils, BigNumber } from "ethers";
 
-export const firstKeys = new Keys();
-export const secondKeys = new Keys();
+const { computePublicKey } = utils;
 
-export const firstDocument : IDIDDocument = {
-    id: 'did:ethr:0x72EFf9faB7876c4c1b6cAe426c121358431758F3',
-    authentication: [
-      {
-        type: 'owner',
-        validity: [Object],
-        publicKey: 'did:ethr:0x72EFf9faB7876c4c1b6cAe426c121358431758F3#owner'
-      }
-    ],
-    created: null,
-    delegates: null,
-    proof: null,
-    publicKey: [
-      {
-        id: 'did:ethr:0x72EFf9faB7876c4c1b6cAe426c121358431758F3#key-owner',
-        type: 'Secp256k1veriKey',
-        controller: '0x72EFf9faB7876c4c1b6cAe426c121358431758F3',
-        publicKeyHex: '0x' + firstKeys.publicKey
-      },
-      {
-        id: 'did:ethr:0x731ac21Aa72c1A6E15243AFBB34cA9CC073D419B#key-owner',
-        type: 'Secp256k1veriKey',
-        controller: '0x731ac21Aa72c1A6E15243AFBB34cA9CC073D419B',
-        publicKeyHex: '0x037cff69ef821172f5df74d3f9406679cc27aba2d96438211538deeb325c9d434d'
-      }
-    ],
-    updated: null,
-    '@context': 'https://www.w3.org/ns/did/v1',
-}
-
-export const secondDocument : IDIDDocument = {
-  id: 'did:ethr:0x0E46cbecF1742DdEb29BBC56F27ad236483443b7',
-  authentication: [
-    {
-      type: 'owner',
-      validity: [Object],
-      publicKey: 'did:ethr:0x0E46cbecF1742DdEb29BBC56F27ad236483443b7#owner'
-    }
-  ],
-  created: null,
-  delegates: null,
-  proof: null,
-  publicKey: [
-    {
-      id: 'did:ethr:0xA533557F26477b1fA7BFa76E6eD5467D9Bc9d633#809ea74d-50c8-451b-a7ce-2fe37b724e0d',
-      type: 'Secp256k1sigAuth',
-      controller: '0xA533557F26477b1fA7BFa76E6eD5467D9Bc9d633',
-      publicKeyHex: 'mynewkey'
-    },
-    {
-      id: 'did:ethr:0x0E46cbecF1742DdEb29BBC56F27ad236483443b7#key-owner',
-      type: 'Secp256k1veriKey',
-      controller: '0x0E46cbecF1742DdEb29BBC56F27ad236483443b7',
-      publicKeyHex: '0x' + secondKeys.publicKey
-    }
-  ],
-  updated: null,
-  '@context': 'https://www.w3.org/ns/did/v1',
-}
-
-export const emptyAuthenticationDocument : IDIDDocument = {
-  id: 'did:ethr:0x0E46cbecF1742DdEb29BBC56F27ad236483443b7',
-  authentication: [],
-  created: null,
-  delegates: null,
-  proof: null,
-  publicKey: [
-    {
-      id: 'did:ethr:0xA533557F26477b1fA7BFa76E6eD5467D9Bc9d633#809ea74d-50c8-451b-a7ce-2fe37b724e0d',
-      type: 'Secp256k1sigAuth',
-      controller: '0xA533557F26477b1fA7BFa76E6eD5467D9Bc9d633',
-      publicKeyHex: '0x' + secondKeys.publicKey
-    }
-  ],
-  updated: null,
-  '@context': 'https://www.w3.org/ns/did/v1',
-}
+export const mockDocument = (
+  identity: Wallet,
+  { withOwnerKey = true }: { withOwnerKey?: boolean } = {}
+): IDIDDocument => {
+  const did = `did:${Methods.Erc1056}:${identity.address}`;
+  const doc: IDIDDocument = {
+    id: did,
+    publicKey: [],
+    authentication: [],
+  } as IDIDDocument;
+  if (withOwnerKey) {
+    doc.authentication.push({
+      type: "owner",
+      validity: BigNumber.from(47),
+      publicKey: `${did}#owner`,
+    });
+    doc.publicKey.push({
+      id: `${did}#${KeyTags.OWNER}`,
+      type: PubKeyType.VerificationKey2018,
+      controller: did,
+      publicKeyHex: computePublicKey(identity.publicKey, true),
+    } as IPublicKey);
+  }
+  return doc as IDIDDocument;
+};
