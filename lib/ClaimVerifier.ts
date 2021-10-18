eb7349426466fb351beb0e4300abe5363e4206c2
Kevin Nzeng Essimengane <kevin.nzeng@energyweb.org>
2021-10-18 09:00:37 +0200

    feat(verifyRole): verifying issuer signature in each claim

--- lib/ClaimVerifier.ts
+++ lib/ClaimVerifier.ts
@@ -1,12 +1,17 @@
-import { Claim, DecodedToken, IRoleDefinition } from "./LoginStrategy.types"
+import { Claim, DecodedToken, IRoleDefinition } from './LoginStrategy.types'
 import * as jwt from 'jsonwebtoken'
+import { IClaims } from '@ew-did-registry/claims'
+import { AuthTokenVerifier } from './AuthTokenVerifier';
+import { IDIDDocument } from '@ew-did-registry/did-resolver-interface';
 
 export class ClaimVerifier {
 
   constructor(
     private readonly claims: Claim[],
     private readonly getRoleDefinition: (namespace: string) => Promise<IRoleDefinition>,
-    private readonly getUserClaims: (did: string) => Promise<Claim[]>) {
+    private readonly getUserClaims: (did: string) => Promise<Claim[]>,
+    private readonly getDidDocument: (did: string) => Promise<IDIDDocument>,
+    ) {
   }
 
   public async getVerifiedRoles(): Promise<{ name: any; namespace: string }[]> {
@@ -61,7 +66,13 @@ export class ClaimVerifier {
       return null
     }
 
+    const issuerClaims = await this.getUserClaims(issuer);
+    const areClaimsValid = await this.verifySignature(issuer, issuerClaims)
+    if (!areClaimsValid){
+      return null
+    }
     if (role.issuer?.issuerType === 'DID') {
+
       if (
         Array.isArray(role.issuer?.did) &&
         role.issuer?.did.includes(issuer)
@@ -75,7 +86,6 @@ export class ClaimVerifier {
     }
 
     if (role.issuer?.issuerType === 'Role') {
-      const issuerClaims = await this.getUserClaims(issuer)
       const issuerRoles = issuerClaims.map((claim) => claim.claimType)
       if (issuerRoles.includes(role.issuer.roleName)) {
         return {
@@ -85,6 +95,22 @@ export class ClaimVerifier {
       }
     }
     return null
+  }
+  
+  private async verifySignature(issuer: string, issuerClaims: Claim[]) : Promise<Boolean>{
+    const didDocument = await this.getDidDocument(issuer);
+    const authenticationClaimVerifier = new AuthTokenVerifier(didDocument);
+    issuerClaims.map(async (claim) => {
+      if (claim.iss !== issuer){
+        return false
+      }
+      const claimToken = claim.issuedToken as string
+      const verifiedIssuer = await authenticationClaimVerifier.verify(claimToken)
+      if (verifiedIssuer !== issuer){
+        return false
+      }
+    })
+    return true
   }
 
 }
