fbe6c71c6d2538d465ed8fb743cdbac7d16b2d47
Artur Sudnik-Hrynkiewicz <artur.sudnik@gmail.com>
2022-09-22 18:48:01 +0200

    fix: fixing a bug when DIDs with no roles are always accepted

--- lib/LoginStrategy.ts
+++ lib/LoginStrategy.ts
@@ -275,10 +275,11 @@ export class LoginStrategy extends BaseStrategy {
       );
       const uniqueRoles = await verifier.getVerifiedRoles();
 
-      if (
+      if (uniqueRoles.length === 0) {
+        return done(undefined, null, 'User does not have any roles.');
+      } else if (
         this.acceptedRoles &&
         this.acceptedRoles.size > 0 &&
-        uniqueRoles.length > 0 &&
         !uniqueRoles.some(({ namespace }) => {
           return this.acceptedRoles.has(namespace);
         })
