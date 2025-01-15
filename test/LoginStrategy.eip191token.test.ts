db55509ab8b4a0924566887570ee741f45b47a3b
jrhender <john.henderson@energyweb.org>
2025-01-15 12:10:20 -0800

    ci: updating node to v16 LTS
    This seems to allow the asset auth test to pass too

--- test/LoginStrategy.eip191token.test.ts
+++ test/LoginStrategy.eip191token.test.ts
@@ -135,7 +135,7 @@ afterAll(async () => {
   await asyncStop();
 });
 
-xit('Verifies asset authentication', async () => {
+it('Verifies asset authentication', async () => {
   // Register an asset
   const assetAddress = await iam.assetService?.registerAsset();
   assert.exists(assetAddress);
